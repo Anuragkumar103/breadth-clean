dc58d87c5355b8a08739c659b64f2870d8d204c2
Anurag Kumar <142084822+Anuragkumar103@users.noreply.github.com>
2026-06-25 22:01:30 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@
 module.exports = {
   // This line enables dark mode. Tailwind will apply 'dark:' styles when
   // the 'dark' class is present on a parent HTML element (e.g., the <html> tag).
-  darkMode: 'class', 
-  
+  darkMode: 'class',  
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
